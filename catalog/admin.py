1bf1423869e9e0a8b7a800e025fd3569178c1405
DaniilKokorev <kokorevdaniil@mail.ru>
2024-04-19 20:33:10 +0700

    создание_моделей

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from catalog.models import Product
+
 # Register your models here.
+# Регистрируйте своих моделей здесь.
+
+# admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name_product', 'description_product')
+    list_filter = ('name_product',)
+    search_fields = ('name_product',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# Создавайте свои модели здесь.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=150, verbose_name='Наименование')
+    description_product = models.TextField(verbose_name='Описание')
+    picture_product = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category_product = models.ForeignKey()
+    price_long = models.IntegerField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
+    def __str__(self):
+        return f'{self.name_product}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
