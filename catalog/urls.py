36000ef89ed7d11b3515511caa52017f1f6755fc
DaniilKokorev <kokorevdaniil@mail.ru>
2024-05-01 13:11:08 +0700

    перевод_контроллеров_с_FBV_на_CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index_shop, contact_shop, product_info
+from catalog.views import ContactView, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", index_shop, name='products_list'),
-    path("contacts/", contact_shop, name='contact_shop'),
-    path("products/<int:pk>", product_info, name='product_info')
+    path("", ProductListView.as_view(), name='products_list'),
+    path("contacts/", ContactView.as_view(), name='contact_shop'),
+    path("products/<int:pk>", ProductDetailView.as_view(), name='product_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
@@ -7,22 +8,46 @@ from catalog.models import Product
 # Создайте свои контроллеры здесь.
 
 
-def index_shop(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "catalog/products_list.html", context)
+class ProductListView(ListView):
+    """CBV класс-контроллер"""
+    model = Product
 
 
-def contact_shop(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(f"{name}, {phone}, {message}")
-    return render(request, "catalog/contact.html")
+# def index_shop(request):
+#     """ FBV функции-контроллер """
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(request, "catalog/product_list.html", context)
 
 
-def product_info(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"item": product}
-    return render(request, "catalog/product_info.html", context)
+class ContactView(TemplateView):
+    """CBV класс-контроллер"""
+    template_name = "catalog/contact.html"
+
+    def post_request(self, request, *args, **kwargs):
+        if request.method == "POST":
+            name = request.POST.get("name")
+            phone = request.POST.get("phone")
+            message = request.POST.get("message")
+            print(f"{name}, {phone}, {message}")
+        return render(request, self.template_name)
+
+
+# def contact_shop(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         phone = request.POST.get("phone")
+#         message = request.POST.get("message")
+#         print(f"{name}, {phone}, {message}")
+#     return render(request, "catalog/contact.html")
+
+
+class ProductDetailView(DetailView):
+    """CBV класс-контроллер"""
+    model = Product
+
+# def product_info(request, pk):
+#     """ FBV функции-контроллер """
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"item": product}
+#     return render(request, "catalog/product_detail.html", context)
