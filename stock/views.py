70a5c77b587754850f642c1dfd6b247c15312437
DaniilKokorev <kokorevdaniil@mail.ru>
2024-05-04 17:04:48 +0700

    фикс4

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from pytils.translit import slugify
