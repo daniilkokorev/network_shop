377ff9c7bc7456e364bf6cac9f750f8af4e1f477
DaniilKokorev <kokorevdaniil@mail.ru>
2024-05-25 16:52:08 +0700

    фикс_модели_пользователя

--- users/models.py
+++ users/models.py
@@ -11,9 +11,16 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
 
-    avatar = models.ImageField(upload_to="user/media", **NULLABLE, verbose_name="Аватар")
+    avatar = models.ImageField(upload_to="users/media", **NULLABLE, verbose_name="Аватар")
     phone = models.CharField(max_length=35, verbose_name='Телефон')
     country = models.CharField(max_length=50, verbose_name='Страна')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
